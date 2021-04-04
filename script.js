e2668ae629041ba23da3644181fe2849c6bb2bfb
Serena Chandler <serenachandler2019@gmail.com>
2021-04-04 09:33:22 -0700

    added 5 day forecast api

--- script.js
+++ script.js
@@ -7,25 +7,25 @@ var weatherApiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city 
 var forcastApiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey;
 
 
+function renderWeather() {
 
+    fetch(weatherApiURL)
+        .then(function (response) {
+            console.log(weatherApiURL);
+            
+            response.json().then(function (data) {
+                console.log(data);
+            
+                $(".cityName").html("<h1>" + data.name + " Weather:")
+                $(".cityTemp").html("<h2>" + "Temperature: " + data.main.temp + " Degrees")
+                $(".windSpeed").html("<h2>" + "Wind Speed: " + data.wind.speed + " MPH")
+                $(".uvIndex").html("<h2>" + "Humidity: " + data.main.humidity)
+                })
 
-fetch(weatherApiURL)
-    .then(function (response) {
-        console.log(weatherApiURL);
-        
-        response.json().then(function (data) {
-            console.log(data);
-          
-            $(".cityName").html("<h1>" + data.name + " Weather:")
-            $(".cityTemp").html("<h2>" + "Temperature: " + data.main.temp + " Degrees")
-            $(".windSpeed").html("<h2>" + "Wind Speed: " + data.wind.speed + " MPH")
-            $(".uvIndex").html("<h2>" + "Humidity: " + data.main.humidity)
-            })
+            
 
-        
-
-});
-
+    });
+}
 
 
 fetch(forcastApiURL)
@@ -33,6 +33,16 @@ fetch(forcastApiURL)
         console.log(forcastApiURL);
         
         response.json().then(function (data) {
-            console.log(data); })
+            console.log(data); 
+
+        $(".day-1").html("<h2>" + "Temp: " + data.list[0].main.temp + " Humidity: " + data.list[0].main.humidity)
+        $(".day-2").html("<h2>" + "Temp: " + data.list[1].main.temp + " Humidity: " + data.list[0].main.humidity)
+        $(".day-3").html("<h2>" + "Temp: " + data.list[2].main.temp + " Humidity: " + data.list[0].main.humidity)
+        $(".day-4").html("<h2>" + "Temp: " + data.list[3].main.temp + " Humidity: " + data.list[0].main.humidity)
+        $(".day-5").html("<h2>" + "Temp: " + data.list[4].main.temp + " Humidity: " + data.list[0].main.humidity)
         
-})
+    })
+        
+}) 
+
+renderWeather()

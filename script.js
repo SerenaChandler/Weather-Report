41621d90fec4202d8bec546e90e2855173046b79
Serena Chandler <serenachandler2019@gmail.com>
2021-04-05 21:08:22 -0700

    reworked some of the js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ var weatherApiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city 
 
 var forcastApiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey;
 
-
+searched.push(city)
 
     fetch(weatherApiURL)
         
@@ -69,12 +69,89 @@ var forcastApiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city
                 $(".cityTemp").html("<h2>" + "Temperature: " + data.main.temp + " Degrees")
                 $(".windSpeed").html("<h2>" + "Wind Speed: " + data.wind.speed + " MPH")
 
-                searched.push(city)
+                
                 console.log(searched)
                 localStorage.setItem("searched", searched)
                 var newSearch = $("<h3>");
                 newSearch.text(city)
+                searched.push(newSearch)
                 searchHistory.append(newSearch)
+                
+                
+                var lat = data.coord.lat
+                var lon = data.coord.lon
+                console.log(lat)
+                var uvIndexAPI = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&cnt={cnt}&appid=" + APIkey;
+                
+
+                fetch(uvIndexAPI)
+                .then(function (response) {
+                    console.log(uvIndexAPI);
+                    
+                    response.json().then(function (data) {
+                        console.log(data);
+                       
+
+                        $(".uvIndex").html("<h2>" + "Humidity: ")
+        
+                        
+        
+        
+                        })
+            });
+
+
+                })
+    });
+
+   
+
+
+
+    fetch(forcastApiURL)
+    .then(function (response) {
+        console.log(forcastApiURL);
+        
+        response.json().then(function (data) {
+            console.log(data); 
+
+        $(".day-1").html("<h2>" + "Temp: " + data.list[4].main.temp + " Humidity: " + data.list[4].main.humidity)
+        $(".day-2").html("<h2>" + "Temp: " + data.list[12].main.temp + " Humidity: " + data.list[1].main.humidity)
+        $(".day-3").html("<h2>" + "Temp: " + data.list[20].main.temp + " Humidity: " + data.list[2].main.humidity)
+        $(".day-4").html("<h2>" + "Temp: " + data.list[28].main.temp + " Humidity: " + data.list[3].main.humidity)
+        $(".day-5").html("<h2>" + "Temp: " + data.list[36].main.temp + " Humidity: " + data.list[4].main.humidity)
+        
+    })
+        
+}); 
+
+}
+
+function renderHistory(newSearch) {
+    var weatherApiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + newSearch + "&units=imperial&appid=" + APIkey;
+
+    var forcastApiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + newSearch + "&units=imperial&appid=" + APIkey;
+
+    console.log(newSearch)
+
+    fetch(weatherApiURL)
+        
+        .then(function (response) {
+            console.log(weatherApiURL);
+            
+            response.json().then(function (data) {
+                console.log(data);
+            
+                $(".cityName").html("<h1>" + data.name + " Weather:")
+                $(".cityTemp").html("<h2>" + "Temperature: " + data.main.temp + " Degrees")
+                $(".windSpeed").html("<h2>" + "Wind Speed: " + data.wind.speed + " MPH")
+
+             
+                console.log(searched)
+              
+                var newSearch = $("<h3>");
+                newSearch.text(city)
+                
                 
 
             //     var lat = data.coord.lat
@@ -127,8 +204,7 @@ var forcastApiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city
 }
 
 
-
 searchBtn.on("click", SearchHandler)
 
 
-searchHistory.on("click", SearchHandler)
+searchHistory.on("click", renderHistory)

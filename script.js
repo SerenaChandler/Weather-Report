ca1f3392bb205858accd076bfab29194056a293a
Serena Chandler <serenachandler2019@gmail.com>
2021-04-05 20:04:43 -0700

    added code to make search history show up

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 var searchBtn = $(".searchButton")
 var APIkey = "15546ce984382d0a081fc48064da1cfe"
 var cityInput = $("#area")
+searchHistory = $("#history")
 console.log(city)
 
-var city = "milpitas"
+var city = ""
 console.log(cityInput)
 
+var searched = []
 
-
-
-
+var i = 0
 var SearchHandler = function (event) {
     event.preventDefault();
+
+    let updateTime = function () {
+        let currentTime = moment().format(' MMMM Do, dddd')
+        $(".currentDate").text(currentTime)
+        }
+    
+    
+        updateTime();
+    
+    
+    
+        setInterval(updateTime, 1000);
+    
+    
+    
   
     var city = cityInput.val()
   
@@ -22,6 +37,12 @@ var SearchHandler = function (event) {
     } else {
       alert('Please enter a city');
     }
+    
+    
+    
+
+    
+
   };
 
 
@@ -33,7 +54,10 @@ var weatherApiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city 
 
 var forcastApiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey;
 
+
+
     fetch(weatherApiURL)
+        
         .then(function (response) {
             console.log(weatherApiURL);
             
@@ -43,9 +67,42 @@ var forcastApiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city
                 $(".cityName").html("<h1>" + data.name + " Weather:")
                 $(".cityTemp").html("<h2>" + "Temperature: " + data.main.temp + " Degrees")
                 $(".windSpeed").html("<h2>" + "Wind Speed: " + data.wind.speed + " MPH")
-                $(".uvIndex").html("<h2>" + "Humidity: " + data.main.humidity)
+
+                searched.push(city)
+                console.log(searched)
+                localStorage.setItem("searched", searched)
+                var newSearch = $("<h3>");
+                newSearch.text(city)
+                searchHistory.append(newSearch)
+                
+
+            //     var lat = data.coord.lat
+            //     var lon = data.coord.lon
+            //     console.log(lat)
+            //     var uvIndexAPI = "http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&cnt={cnt}&appid=" + APIkey;
+                
+
+            //     fetch(uvIndexAPI)
+            //     .then(function (response) {
+            //         console.log(uvIndexAPI);
+                    
+            //         response.json().then(function (data) {
+            //             console.log(data);
+                       
+
+            //             $(".uvIndex").html("<h2>" + "Humidity: ")
+        
+                        
+        
+        
+            //             })
+            // });
+
+
                 })
     });
+
+   
 
 
 
@@ -56,15 +113,15 @@ var forcastApiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city
         response.json().then(function (data) {
             console.log(data); 
 
-        $(".day-1").html("<h2>" + "Temp: " + data.list[0].main.temp + " Humidity: " + data.list[0].main.humidity)
-        $(".day-2").html("<h2>" + "Temp: " + data.list[1].main.temp + " Humidity: " + data.list[1].main.humidity)
-        $(".day-3").html("<h2>" + "Temp: " + data.list[2].main.temp + " Humidity: " + data.list[2].main.humidity)
-        $(".day-4").html("<h2>" + "Temp: " + data.list[3].main.temp + " Humidity: " + data.list[3].main.humidity)
-        $(".day-5").html("<h2>" + "Temp: " + data.list[4].main.temp + " Humidity: " + data.list[4].main.humidity)
+        $(".day-1").html("<h2>" + "Temp: " + data.list[4].main.temp + " Humidity: " + data.list[4].main.humidity)
+        $(".day-2").html("<h2>" + "Temp: " + data.list[12].main.temp + " Humidity: " + data.list[1].main.humidity)
+        $(".day-3").html("<h2>" + "Temp: " + data.list[20].main.temp + " Humidity: " + data.list[2].main.humidity)
+        $(".day-4").html("<h2>" + "Temp: " + data.list[28].main.temp + " Humidity: " + data.list[3].main.humidity)
+        $(".day-5").html("<h2>" + "Temp: " + data.list[36].main.temp + " Humidity: " + data.list[4].main.humidity)
         
     })
         
-}) 
+}); 
 
 }
 
